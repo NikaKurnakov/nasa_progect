f31b89e53739b33c668d8b0edd38e538a2c9638f
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2023-05-13 17:30:51 +0300

    Update get_epic_image.py

--- get_epic_image.py
+++ get_epic_image.py
@@ -1,36 +1,35 @@
 import requests
 import argparse
 import load_image
+from datetime import datetime
 
 
 def get_epic_picture(epic_url, args):
-    if args.token_epic:
-        params = {
-            'apy_key': args.token_epic
-        }
-        response = requests.get(epic_url, params=params)
-    else:
-        response = requests.get(epic_url)
+    params = {
+        'api_key': args.token_epic
+    }
+    response = requests.get(epic_url, params=params)
     response_json = response.json()
-    epic_list = []
-    for image in range(10):
-        if response_json[image]['image'] and response_json[image]['date']:
-            image_name = response_json[image]['image']
-            image_date = response_json[image]['date']
-            image_date_split = image_date[0:10].replace('-', '/')
-            epic_image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date_split}/png/{image_name}.png?api_key=DEMO_KEY"
-            epic_list.append(epic_image_url)
-    return epic_list
-
+    epic_images = []
+    number_images = int(args.numb)
+    for image in range(number_images):
+        image_name = response_json[image]['image']
+        image_date_json = response_json[image]['date']
+        if image_name and image_date_json:
+            image_date_format = datetime.fromisoformat(image_date_json)
+            image_date = datetime.date(image_date_format).strftime("%Y/%m/%d")
+            epic_image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png"
+            epic_images.append(epic_image_url)
+    return epic_images
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--token_epic", help="token epic")
-    epic_url = 'https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY'
+    parser.add_argument("--token_epic", help="token epic", required=True)
+    parser.add_argument("--numb", help="numb picture")
     args = parser.parse_args()
-    image_epic_list = get_epic_picture(epic_url, args)
-    print(image_epic_list)
-    load_image.get_images("epic_image", image_epic_list)
+    epic_url = 'https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY'
+    epic_function = get_epic_picture(epic_url, args)
+    load_image.get_images("epic_image", epic_function, parser)
 
 
 if __name__ == '__main__':
