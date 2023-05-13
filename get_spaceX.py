e6634ba46e67a32d74da714be22f697d5447259d
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2023-05-13 17:32:54 +0300

    Update get_spaceX.py

--- get_spaceX.py
+++ get_spaceX.py
@@ -4,29 +4,31 @@ import argparse
 import load_image
 
 
-def get_one_launch(url, args):
-    if args.id:
+def get_one_launch(spaceX_url, parser, args):
+    args_dict = vars(parser.parse_args())
+    if "id" in args_dict:
         params = {
             'id': args.id
         }
-        response = requests.get(url, params=params)
+        response = requests.get(spaceX_url, params=params)
     else:
-        response = requests.get(url)
-    data = json.dumps(response.json())
+        response = requests.get(spaceX_url)
+    data_json = response.json()
+    data = json.dumps(data_json)
     json_file = json.loads(data)
     for image in range(len(json_file)):
-        if json_file[-image]['links']['flickr']['original']:
-            return json_file[-image]['links']['flickr']['original']
+        image_path = json_file[-image]['links']['flickr']['original']
+        if image_path:
+            return image_path
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--id", help="id интересующей картинки")
     args = parser.parse_args()
-    url = "https://api.spacexdata.com/v5/launches/"
-    image_spaceX_list = get_one_launch(url, args)
-    print(image_spaceX_list)
-    load_image.get_images('spaceX_image', image_spaceX_list)
+    spaceX_url = "https://api.spacexdata.com/v5/launches/"
+    image_spaceX_list = get_one_launch(spaceX_url, parser, args)
+    load_image.get_images('spaceX_image', image_spaceX_list, parser)
 
 
 if __name__ == '__main__':
