3d992cbd6b0915c8e627f132f222c4f373cc2934
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2023-05-13 17:32:24 +0300

    Update get_nasa_image.py

--- get_nasa_image.py
+++ get_nasa_image.py
@@ -4,35 +4,30 @@ import argparse
 import load_image
 
 
-
-def nasa_apod(nasa_url, args):
-    if args.token_nasa:
-        params = {
-            'start_date': "2023-03-15",
-            'end_date': "2023-04-15",
-            'api_key': args.token_nasa
-        }
-        response = requests.get(nasa_url, params=params)
-    else:
-        response = requests.get(nasa_url)
+def get_nasa_apod_picture(nasa_url, args):
+    params = {
+        'start_date': "2023-03-15",
+        'end_date': "2023-04-15",
+        'api_key': args.token_nasa
+    }
+    response = requests.get(nasa_url, params=params)
     picture_today = response.json()
-    date_pictures = json.dumps(response.json())
+    date_pictures = json.dumps(picture_today)
     json_pictures = json.loads(date_pictures)
-    image_list = []
+    images = []
     for image in range(len(picture_today)):
         if json_pictures[image]['media_type'] == 'image':
-            image_list.append(json_pictures[image]['url'])
-    return image_list
+            images.append(json_pictures[image]['url'])
+    return images
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--token_nasa", help="token nasa")
-    nasa_url = 'https://api.nasa.gov/planetary/apod'
+    parser.add_argument("--token_nasa", help="token nasa", required=True)
     args = parser.parse_args()
-    image_nasa_list = nasa_apod(nasa_url, args)
-    print(image_nasa_list)
-    load_image.get_images("nasa_image", image_nasa_list)
+    nasa_url = 'https://api.nasa.gov/planetary/apod'
+    images_nasa = get_nasa_apod_picture(nasa_url, args)
+    load_image.get_images("nasa_image", images_nasa, parser)
 
 
 if __name__ == '__main__':
