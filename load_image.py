80443fcd41a964f6a57c28cff8bf6f52ab73e977
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2023-05-15 20:06:02 +0300

    Update load_image.py

--- load_image.py
+++ load_image.py
@@ -5,15 +5,15 @@ import os
 def get_images(filename, images, parser):
     os.makedirs("images", exist_ok=True)
     args = parser.parse_args()
-    args_dict = vars(parser.parse_args())
+    args_parser = vars(parser.parse_args())
     for image_number, image in enumerate(images):
         filename1 = os.path.join("images", f"{filename}_{image_number}.png")
-        if "token_epic" in args_dict:
+        if "token_epic" in args_parser:
             params = {
                 'api_key': args.token_epic,
             }
             response = requests.get(image, params=params)
-        elif "token_nasa" in args_dict:
+        elif "token_nasa" in args_parser:
             params = {
                 'api_key': args.token_nasa
             }
