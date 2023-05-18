239d9fd32a9d93e8f608b7e61b6fe11581c0370d
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2023-05-18 19:37:51 +0300

    Update get_epic_image.py

--- get_epic_image.py
+++ get_epic_image.py
@@ -4,32 +4,34 @@ import load_image
 from datetime import datetime
 
 
-def get_epic_picture(args):
+def get_epic_picture(numb, params):
     epic_url = 'https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY'
-    params = {
-        'api_key': args.token_epic
-    }
     response = requests.get(epic_url, params=params)
-    unpacked_response = response.json()
-    epic_images = []
-    images_number = int(args.numb)
-    for image in range(images_number):
-        image_name = unpacked_response[image]['image']
-        image_date_json = unpacked_response[image]['date']
-        if image_name and image_date_json:
-            image_date_format = datetime.fromisoformat(image_date_json)
-            image_date = datetime.date(image_date_format).strftime("%Y/%m/%d")
-            epic_image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png"
-            epic_images.append(epic_image_url)
-    return epic_images
+    if response.ok:
+        unpacked_response = response.json()
+        epic_images = []
+        images_number = numb
+        for image in range(images_number):
+            image_name = unpacked_response[image]['image']
+            image_date_json = unpacked_response[image]['date']
+            if image_name and image_date_json:
+                image_date_format = datetime.fromisoformat(image_date_json)
+                image_date = datetime.date(image_date_format).strftime("%Y/%m/%d")
+                epic_image_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png"
+                epic_images.append(epic_image_url)
+        return epic_images
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--token_epic", help="token epic", required=True)
-    parser.add_argument("--numb", help="numb picture")
+    parser.add_argument("--numb", help="numb picture", type=int, required=True)
     args = parser.parse_args()
-    epic_function = get_epic_picture(epic_url, args)
-    load_image.get_images("epic_image", epic_function, parser)
+    numb = args.numb
+    params = {
+        'api_key': args.token_epic
+    }
+    epic_function = get_epic_picture(numb, params)
+    load_image.get_images("epic_image", epic_function, params)
 
 
 if __name__ == '__main__':
